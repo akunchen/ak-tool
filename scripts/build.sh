8f41d49cf2fde0f7915997219276c8d71a2bf920
阿坤 <>
2018-05-02 14:03:37 +0800

    更新忽略打包文件

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
-cd ..
+
+baseDir=..
+packageDir=dist/package
+appPath=$packageDir/ak-tool-darwin-x64/ak-tool.app
+
+# 进入目录
+cd $baseDir
+
+# 运行编译
 node .electron-vue/build.js
-electron-packager . ak-tool --overwrite --out=dist/package
-#electron-builder .
-open dist/package/ak-tool-darwin-x64/electron-vue.app
+
+# 打包
+ignore="
+--ignore=.idea
+--ignore=.electron-vue
+"
+electron-packager . ak-tool --overwrite --out=$packageDir $ignore
+
+# 打开打包好的app
+open $appPath
